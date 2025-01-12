9c1b116efd18028e16131df60403006fe7601812
Tamara Sequeiros <tamara.sequeiros@gmail.com>
2025-01-12 19:13:25 +0100

    Initial Commit

---
+++ TabooGame/TabooGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabooGameApp.swift
+//  TabooGame
+//
+//  Created by Tamara on 12/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TabooGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TabooGameTests/TabooGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabooGameTests.swift
+//  TabooGameTests
+//
+//  Created by Tamara on 12/01/2025.
+//
+
+import Testing
+@testable import TabooGame
+
+struct TabooGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
